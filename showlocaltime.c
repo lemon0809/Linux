4d5ac5eca49c06375a2c6ef9fc4a8fcb393155a5
lemon <lemon@localhost.localdomain>
2016-09-25 18:50:34 +0800

    first

---
+++ showlocaltime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+int main()
+{
+	time_t timep;
+	struct tm *p;
+	for(;;)
+	{
+	time(&timep);
+ 	p = localtime(&timep);	//取得当地时间
+	printf("%d:%d:%d\n",p->tm_hour,p->tm_min,p->tm_sec);
+	sleep(1);						//睡一秒
+	printf("\033[1A"); 	//先回到上一行
+	printf("\033[K");		//清除该行
+	}
+	
+	return 0;
+}
